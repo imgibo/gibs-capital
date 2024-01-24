c3fb3265465308b26366229fab035eaa84a67fd8
imgibo <gibscreative@gmail.com>
2024-01-24 21:37:08 +0800

    continue with restful api

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,13 @@ import express from 'express';
 const PORT = 3001;
 const app = express();
 
+app.use(express.json());
+
+function generateId() {
+    const maxId = savings.length > 0 ? Math.max(...savings.map((n) => n.id)) : 0;
+    return maxId + 1;
+};
+
 let savings = [
     {
         id: 1,
@@ -37,6 +44,30 @@ app.get("/savings/:id", (req, res) => {
     res.json(amount);
 });
 
+app.delete("/savings/:id", (req, res) => {
+    const id = Number(req.params.id);
+    savings = savings.filter((amount) => amount.id !== id);
+
+    res.status(204).end();
+});
+
+app.post("/savings", (req, res) => {
+    const body = req.body;
+
+    if (!body.amount) {
+        return res.status(400).json({error: "Kindly deposit an amount"});
+    };
+
+    const amount = {
+        name: body.name,
+        amount: body.amount,
+        id: generateId(),
+    };
+
+    savings = savings.concat(amount);
+
+    res.status(201).json(amount);
+});
 
 app.listen(PORT, () => {
     console.log(`The server is now running on port ${PORT}`)
